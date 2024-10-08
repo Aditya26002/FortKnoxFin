99f64e9da8467e369d448ab1b0ad3ab55ba37631
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-10-08 21:51:35 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { IBM_Plex_Serif, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  variable: "--font-ibm-plex-serif",
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "FortKnoxFin",
+  description: "Built on trust, your financial fortress",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
