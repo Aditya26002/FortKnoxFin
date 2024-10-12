968cfee72e50fafe6a65381d10f17f486ff1453a
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-10-12 16:47:19 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotaBalanceBox from "@/components/TotaBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Aditya",
+    lastName: "Kumar",
+    email: "asfd@njn",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and amnage your account and transactions efficiently."
+          />
+          <TotaBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={12156.21}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 12156.21 }, { currentBalance: 156.23 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/MobileNav.tsx
@@ -0,0 +1,78 @@
+"use client";
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+
+const MobileNav = ({ user }: MobileNavProps) => {
+  const pathName = usePathname();
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side={"right"} className="border-none bg-white">
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav
+                className="flex flex-col h-full gap-6 pt-16
+                text-white"
+              >
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathName === item.route ||
+                    pathName.startsWith(`${item.route}/`);
+                  return (
+                    <Link
+                      href={item.route}
+                      key={item.label}
+                      className={cn("mobilenav-sheet_close", {
+                        "bg-bank-gradient": isActive,
+                      })}
+                    >
+                      <div className="relative size-6">
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-[3] invert-0": isActive,
+                          })}
+                        />
+                      </div>
+                      <p
+                        className={cn("text-16 font-semibold text-black-2", {
+                          "text-white": isActive,
+                        })}
+                      >
+                        {item.label}
+                      </p>
+                    </Link>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
